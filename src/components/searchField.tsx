37dd26777d609a98439153fd42e1e3d9121deef9
Florian de Ghellinck <f.deghellinck@gmail.com>
2023-07-23 18:11:32 +0200

    feat: searchbar implementation & styling

--- src/components/searchField.tsx
+++ src/components/searchField.tsx
@@ -1,10 +1,29 @@
-import { FC } from "react";
+import { SearchSVG } from "@/assets/search";
+import { Dispatch, FC, SetStateAction, useEffect } from "react";
+import { useForm } from "react-hook-form";
 
-export const SearchBar: FC<{}> = () => {
+export const SearchBar: FC<{ setSearchValue?: Dispatch<SetStateAction<string | null>> }> = ({ setSearchValue }) => {
+  const { register, watch } = useForm();
+
+  const searchInput = watch("searchInput");
+
+  useEffect(() => {
+    if (setSearchValue) setSearchValue(searchInput);
+  }, [searchInput, setSearchValue]);
+
   return (
-    <>
-      <div> search logo </div>
-      <div className="bg-red-500 text-right">search field</div>
-    </>
+    <form className="w-full md:w-1/2 lg:w-1/3">
+      <div className="relative">
+        <div className="pointer-events-none absolute inset-y-0 right-0 pr-3 flex items-center">
+          <SearchSVG className="fill-primary-blue w-5 h-5 " />
+        </div>
+        <input
+          {...register("searchInput")}
+          type="text"
+          placeholder="Enter name or number.."
+          className="w-full rounded-lg border-2 border-primary-blue placeholder:text-gray-300 py-2 px-2 focus:outline-none "
+        />
+      </div>
+    </form>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import { SearchBar } from "@/components/searchField";
 import { Typography } from "@/components/typography";
 import { Usercart } from "@/components/usercart";
+import includes from "lodash/includes";
+import toLower from "lodash/toLower";
+import filter from "lodash/filter";
 import map from "lodash/map";
+import { useState } from "react";
+import replace from "lodash/replace";
 
 export default function Home() {
   const usersToTest = [
@@ -10,18 +15,30 @@ export default function Home() {
     { id: "1aze654", firstName: "Schtroumpf", lastName: "Farceur", phoneNumber: "+32 472 75 60 44" },
   ];
 
+  const [searchValue, setSearchValue] = useState<string | null>("");
+
+  const usersFiltered = filter(usersToTest, (user) => {
+    const formatedSearchValue = replace(toLower(searchValue as string), / /g, "");
+
+    return (
+      includes(replace(toLower(user.firstName), / /g, ""), formatedSearchValue) ||
+      includes(replace(toLower(user.lastName), / /g, ""), formatedSearchValue) ||
+      includes(replace(user.phoneNumber, / /g, ""), formatedSearchValue)
+    );
+  });
+
   return (
-    <main className="flex h-screen w-screen flex-col items-center justify-between p-4 bg-[#1D5D9B]">
-      <div className="flex items-center flex-col h-full w-full gap-y-12">
+    <main className="flex h-screen w-screen flex-col items-center justify-between p-4 bg-primary-blue">
+      <div className="flex items-center flex-col h-full w-full gap-y-12 pt-8">
         <Typography.h1>Phonebook</Typography.h1>
 
         <div className="h-full w-full sm:h-5/6 sm:w-5/6 lg:h-3/4 lg:w-3/4 bg-white rounded-2xl shadow-2xl gap-y-4 p-10 flex flex-col">
-          <div className="flex justify-end w-full gap-x-4 border-b-2">
-            <SearchBar />
+          <div className="flex justify-end w-full border-b-2 pb-2">
+            <SearchBar setSearchValue={setSearchValue} />
           </div>
           <div className="flex items-center ">
             <ul className="">
-              {map(usersToTest, (eachUser: User) => {
+              {map(usersFiltered, (eachUser: User) => {
                 return (
                   <li key={eachUser.id}>
                     <Usercart user={eachUser} />
