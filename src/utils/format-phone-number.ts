75fe90bc6d76152c4bcdb942744ad7b067ed2da1
Florian de Ghellinck <f.deghellinck@gmail.com>
2023-07-25 10:24:16 +0200

    style: rendering as belgian numbers in formatnumber function

--- src/utils/format-phone-number.ts
+++ src/utils/format-phone-number.ts
@@ -6,7 +6,7 @@ export const formatNumberForDB = (number: string) => {
 
 export const formatNumberForContactTable = (number: string) => {
   const lastSixNumbers = number.slice(-6);
-  const middleNumbers = number.slice(-8, -6);
-  const firstNumbers = number.slice(0, -8);
+  const middleNumbers = number.slice(-9, -6);
+  const firstNumbers = number.slice(0, -9);
   return `${firstNumbers} ${middleNumbers} ${lastSixNumbers}`;
 };
